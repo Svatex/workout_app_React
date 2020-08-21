b44cb365e3ebd0221f77c5ba0910289bbc609480
Krystof Svaton <Svatykr@gmail.com>
2020-08-21 07:12:51 +0200

    fetch changed from app to comp

--- src/App.js
+++ src/App.js
@@ -1,28 +1,21 @@
 import React, { useState, useEffect } from "react";
 import BodyMap from "./BodyMap";
-import SingleExcercise from './SingleExcercise'
+import SingleExcercise from "./SingleExcercise";
 
 function App() {
-  const [ExcData, setExcData] = useState([]);
+  //const [ExcData, setExcData] = useState([]);
 
-  useEffect(() => { fetch ("https://swapi.dev/api/people/")
-      .then (Response => Response.json())
-      .then (Data => {setExcData(Data.results)})
-  }, [])
-
-  console.log(ExcData[0])
+ 
 
   return (
     <div className="App">
       <BodyMap />
       <div className="excercise-container">
-      <div>
-        <h2>Select your excercise</h2>
-        <SingleExcercise excerciseName='{ExcData[0].name}' />
+        <div>
+          <h2>Select your excercise</h2>
+          <SingleExcercise />
+        </div>
       </div>
-    </div>
-    
-      
     </div>
   );
 }
--- src/SingleExcercise.js
+++ src/SingleExcercise.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
 
+function SingleExcercise() {
+  const [ExcData, setExcData] = useState([]);
 
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts?_limit=3")
+      .then((Response) => Response.json())
+      .then((Data) => {
+        setExcData(Data);
+      });
+  }, []);
 
-function singleExcercise (props) {
- return (
+  console.log(ExcData);
+
+  return (
     <div className="excercise">
-          <h1>{props.excerciseName}</h1>
-          <h1>{props.description}</h1>
+      <h1>{ExcData.userID}</h1>
+      <h1>{ExcData.body}</h1>
     </div>
- )
+  );
 }
 
-export default singleExcercise
+export default SingleExcercise;
